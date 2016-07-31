8aa39af8ae42e6af26a0e644942b23a862a4d201
BoTung <bver49@gmail.com>
2016-07-31 20:23:57 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   /* toastr option */
   toastr.options = {
     "closeButton": true,
@@ -8,5 +8,75 @@ $(document).ready(function(){
     "timeOut": "3000",
     "extendedTimeOut": "1000"
   }
+  var A = 1.63;
+  var B = 2.38;
+  var C = 3.52;
+  var D = 4.61;
 
+  $('#lastm').on('keyup', function() {
+    var t = parseFloat($("#thism").val());
+    var diff = t - l;
+    $("#degree").val(diff);
+    counting();
+  });
+  $('#thism').on('keyup', function() {
+    var l = parseFloat($("#lastm").val());
+    var t = parseFloat($("#thism").val());
+    var diff = t - l;
+    $("#degree").val(diff);
+    counting();
+  });
+  $('.counting').on('keyup', function() {
+    counting();
+  });
+
+  function counting() {
+    var degree = parseFloat($("#degree").val());
+    var fee = 0;
+    var al = parseFloat($('input[name="AL"]').val());
+    var au = parseFloat($('input[name="AU"]').val());
+    var bl = parseFloat($('input[name="BL"]').val());
+    var bu = parseFloat($('input[name="BU"]').val());
+    var cl = parseFloat($('input[name="CL"]').val());
+    var cu = parseFloat($('input[name="CU"]').val());
+    var dl = parseFloat($('input[name="DL"]').val());
+    var du = parseFloat($('input[name="DU"]').val());
+    switch (true) {
+      case (degree <= au):
+        fee = A * degree;
+        break;
+      case (degree <= bu && degree > au):
+        fee = A * (au) + B * (degree - au);
+        break;
+      case (degree <= cu && degree > bu):
+        fee = A * (au) + B * (bu - au) + C * (degree - bu);
+        break;
+      case (degree <= du && degree > cu):
+        fee = A * (au) + B * (bu - au) + C * (cu - bu) + D * (degree - cu);
+        break;
+      default:
+        fee = 0;
+        break;
+    }
+    fee = fee.toFixed(2);
+    $('#fee').html(fee);
+  }
+  $('#check').on('click', function() {
+    if (this.checked) {
+      A = 1.63;
+      B = 2.10;
+      C = 2.89;
+      D = 3.79;
+    } else {
+      A = 1.63;
+      B = 2.38;
+      C = 3.52;
+      D = 4.61;
+    }
+    $("#A").html(A);
+    $("#B").html(B);
+    $("#C").html(C);
+    $("#D").html(D);
+    counting();
+  });
 });
